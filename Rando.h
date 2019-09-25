27282574f9cc07be4b56345735096b2b06cd659d
b-tudor <btudor@mail.usf.edu>
2019-09-25 16:32:34 -0400

    Initial source commit

---
+++ Rando.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef RANDO_H
+#define RANDO_H
+
+#include <random>
+
+class Rando
+{
+public:
+	static void seed( unsigned int seed ) { mt.seed(seed); }
+
+	static double rand()        { return uniform_distribution(mt); }
+	static double rand_normal() { return  normal_distribution(mt); }
+	
+private:
+	static std::mt19937 mt;
+	static std::normal_distribution<double>       normal_distribution;
+	static std::uniform_real_distribution<double> uniform_distribution;
+};
+#endif
